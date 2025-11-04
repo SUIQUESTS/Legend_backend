5d3abd0710b2b84ffe3680d2c2e81e8627c9a476
eric allibalogun <allieric28@gmail.com>
2025-11-04 18:22:15 +0100

    challenge implementation in progress

--- models/Submission.js
+++ models/Submission.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const submissionSchema = new mongoose.Schema({
     challenge:{type:mongoose.Schema.Types.ObjectId,ref:"Challenge",required: true},
 
-    participantAddress:{type:String,required:true},
+    participant_address:{type:String,required:true},
 
-    submissionLink:{type:String,required:true},
+    submission_link:{type:String,required:true},
 
     submittedAt:{type:Date,default:Date.now},
     
