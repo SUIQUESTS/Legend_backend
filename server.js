aa9b86a9c2d9dbbd39fa89bb6be1fd8af1a8b38c
eric allibalogun <allieric28@gmail.com>
2025-11-04 11:26:45 +0100

    made changes

--- server.js
+++ server.js
@@ -11,14 +11,9 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/users", userRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 10000,
-})
-.then(() => console.log("✅ MongoDB Connected"))
-.catch(err => console.error("❌ MongoDB Connection Error:", err));
-
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error(err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
