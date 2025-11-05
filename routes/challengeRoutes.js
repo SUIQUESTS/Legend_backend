dc6f7a71ba3384cc86b77364ebb69656d63c9d12
eric allibalogun <allieric28@gmail.com>
2025-11-05 13:06:19 +0100

    chages made in challenge implementation

--- routes/challengeRoutes.js
+++ routes/challengeRoutes.js
@@ -9,6 +9,6 @@ router.get("/:id", challengeController.getChallengeById);
 router.post("/:id/submit", challengeController.submitToChallenge);
 router.get("/user/:walletAddress", challengeController.getSubmissionsByUser);
 router.put("/:id/complete", challengeController.completeChallenge);
-router.put("/select-winner", selectWinner);
+router.put("/select-winner", challengeController.selectWinner);
 
 export default router;
